a0fdf12e1c12b31c82f3c082c1914c77c23e7fc0
Ramiro <ramirobarbagelata@gmail.com>
2022-08-19 15:47:44 -0300

    Creacion de NavBar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './componets/NavBar';
+
 
 function App() {
+
+
+
   return (
+    
     <div className="App">
-      <h1>Bienvenido a E-commerce</h1>
-      <p>Todo lo que buscas en un solo lugar</p>
-      <h4>Explorar</h4>
-      <div className='menu'>
+      <NavBar />
+      <div>
+        <h1>Bienvenido a Nike Running Shoes</h1>
+        <p>Todo lo que buscas en un solo lugar</p>
+        <h4>Explorar</h4>
+      </div>
+      
+      {/* <div className='menu'>
         <li>Home</li>
         <li>Categorias</li>
         <li>Ofertas</li>
         <li>Contacto</li>
-      </div>
+      </div> */}
     </div>
     
   );
