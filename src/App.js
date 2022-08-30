2b9c5a4c804bf7590182c13f406254997598e0e8
Ramiro <ramirobarbagelata@gmail.com>
2022-08-30 00:23:57 -0300

    Contador con botón

--- src/App.js
+++ src/App.js
@@ -4,21 +4,33 @@ import ItemListContainer from './componets/ItemListContainer';
 import RunPhoto from './assets/RunPhoto.png';
 import { ThemeProvider } from '@mui/material';
 import theme from './MuiTheme';
+import Counter from './componets/ItemCount'
+
+
 
 
 
 function App() {
   const greeting = 'Bienvenido a Nike Running Shoes';
+
+  const stock = 7;
+  const initial = 1;
+  let totalShop = 0;
+  const onAdd = (quantity) => totalShop === quantity;
+  
+
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
         <NavBar />
         <ItemListContainer greeting={greeting}/>
         <div>
-          <img src={RunPhoto} alt="" />
+          {/* <img src={RunPhoto} alt="" /> */}
         </div>
+        <Counter stock={stock} initial={initial} onAdd={onAdd}/>
       </div>
     </ThemeProvider>
+    
     
   );
 }
---
+++ src/componets/ItemCount.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+import Stack from '@mui/material/Stack';
+
+
+
+const Counter = ({ stock, initial, onAdd }) => {
+    const [counter, setCounter] = useState(initial);
+
+    return (
+        <Card sx={{ maxWidth: 345 }} style={{borderRadius: 10}}>
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="300"
+                    image="https://i.postimg.cc/B635TYDq/nike-Shoe01.png"
+                    alt="Nike Free"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Nike Free RN Flyknit 2018
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Diseñado para carreras cortas cuando quieres una sensación de estar descalzo,
+                        el Nike Free RN Flyknit 2018 se siente muy ligero y flexible.
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Stack direction="row" spacing={8}>
+
+                    <Button variant="outlined" onClick={() => {
+                        counter > initial ? setCounter(counter - 1) : alert('Por lo menos selecciona un producto')
+                    }}> - </Button>
+
+                    <Button size="large" color="secondary">
+                        {counter}
+                    </Button>
+
+                    <Button variant="outlined" onClick={() => {
+                        counter < stock ? setCounter(counter + 1) : alert('No hay stock suficiente')
+                    }}> + </Button>
+
+                </Stack>
+            </CardActions>
+
+            <Button size="large" variant="outlined" href="#outlined-buttons" style={{ margin: 20 }} onClick={() => {
+                onAdd(counter);
+                setCounter(initial);
+            }}> Comprar ahora </Button>
+
+        </Card>
+    );
+}
+
+export default Counter
